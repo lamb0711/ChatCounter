77c2b6dc036aba383d841f48b3580b14757d45ce
Yang sujin <21500394@handong.edu>
2018-05-17 01:23:37 +0900

    instantiation RedundancyChecker

--- src/main/java/edu/handong/csee/java/hw3/Main.java
+++ src/main/java/edu/handong/csee/java/hw3/Main.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class Main {
 	public static ArrayList<String> fileLine = new ArrayList<String>();
 	public static ArrayList<String> messageLine = new ArrayList<String>();
+	public static ArrayList<String> message = new ArrayList<String>();
 	
 	public static void main(String[] args) {
 		FileLoader fi = new FileLoader();
 		MessageParser me = new MessageParser();
+		RedundancyChecker re = new RedundancyChecker();
 		
-		fi.readFile(fileLine);
-		me.saveOnlyMessage(fileLine, messageLine);
+		fileLine = fi.readFile(fileLine);
+		messageLine = me.saveOnlyMessage(fileLine, messageLine);
+		message = re.removeRedundancy(messageLine, message);
 
 	}
 
--- src/main/java/edu/handong/csee/java/hw3/RedundancyChecker.java
+++ src/main/java/edu/handong/csee/java/hw3/RedundancyChecker.java
@@ -4,18 +4,21 @@ import java.util.ArrayList;
 
 public class RedundancyChecker {
 	
-	ArrayList<String> removeRedundancy(ArrayList<String> MessageLine){
+	ArrayList<String> removeRedundancy(ArrayList<String> messageLine,ArrayList<String> message){
 		
-		for(int i=0; i<MessageLine.size()-1; i++) {
-			String line1 = MessageLine.get(i);
-			String line2 = MessageLine.get(i+1);
+		for(int i=0; i<messageLine.size()-1; i++) {
+			String line1 = messageLine.get(i);
+			String line2 = messageLine.get(i+1);
 			
 			if(line1.equals(line2)) {
-				
+				message.add(line1);
+				continue;
 			}
+			message.add(line1);
+			
 		}
 		
-		return MessageLine;
+		return message;
 	}
 
 }
